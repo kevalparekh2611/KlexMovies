89e06d193c38639faebbfa116901efab3a0f8c84
kevalparekh2611 <47861201+kevalparekh2611@users.noreply.github.com>
2021-05-06 12:36:06 +0530

    added files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>KlexMovies</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    color: '#fff',
+    fontSize: 40,
+    fontWeight:'bold'
+  },
+});
